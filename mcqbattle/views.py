ab06baea1c03915b548cdd8268174544b5d21197
Swaraj <swarajbandhi0000@gmail.com>
2024-07-09 15:02:07 +0530

    just testing the server

--- mcqbattle/views.py
+++ mcqbattle/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello, world. You are at Swaraj's homepage")
+    return HttpResponse("Hello, world. You are at himanshu")
